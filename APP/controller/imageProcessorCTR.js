54a61d8b56b142a5c085b8fb295721783c658b49
epson <121927532@qq.com>
2012-11-18 21:56:23 +0800

    增加图像滤波功能

--- APP/controller/imageProcessorCTR.js
+++ APP/controller/imageProcessorCTR.js
@@ -7,13 +7,72 @@ jQuery(function(){
             name: "imageProcessorCTR",
             el: $("#imageProcessor"),
             panel: $("#ImagePanel"),
-            elements: [$("#grayLevel"),$("#filter")],
-            filters: null,
+            elements: [$("#grayLevel"), $("#typeOfFilter")],
+            filtersProcessor: null,
             init: function() {
-                this.filters = {
-                    averageFilter: [    0.111, 0.111, 0.111,
-                                        0.111, 0.111, 0.111,
-                                        0.111, 0.111, 0.111  ]
+                this.filtersProcessor = {
+                    averageFilter: function(data, index, width, sizeOfFilter) {
+                        var range = Math.floor(sizeOfFilter / 2);
+                        var result = 0;
+                        for( var i = -range; i <= range; i ++ ) {
+                            for( var j = -range; j <= range; j ++ ) {
+                                result = result + data[index + (i * width + j) * 4] * 0.111;
+                            }
+                        }
+                        return result;
+                    },
+                    minimalFilter: function(data, index, width, sizeOfFilter) {
+                        var range = Math.floor(sizeOfFilter / 2);
+                        var min = 9999999;
+                        for( var i = -range; i <= range; i ++ ) {
+                            for( var j = -range; j <= range; j ++ ) {
+                                min = data[index + (i * width + j) * 4] < min ? data[index + (i * width + j) * 4] : min;
+                            }
+                        }
+                        return min;
+                    },
+                    midValueFilter: function(data, index, width, sizeOfFilter) {
+                        var range = Math.floor(sizeOfFilter / 2);
+                        var array = [];
+                        for( var i = -range; i <= range; i ++ ) {
+                            for( var j = -range; j <= range; j ++ ) {
+                                array.push(data[index + (i * width + j) * 4]);
+                            }
+                        }
+
+                        var algorithm = new Algorithm();
+                        algorithm.quickSort(array, 0, array.length - 1);
+
+                        return array[Math.floor(array.length / 2)];
+                    },
+                    maximalFilter: function(data, index, width, sizeOfFilter) {
+                        var range = Math.floor(sizeOfFilter / 2);
+                        var max = 0;
+                        for( var i = -range; i <= range; i ++ ) {
+                            for( var j = -range; j <= range; j ++ ) {
+                                max = data[index + (i * width + j) * 4] > max ? data[index + (i * width + j) * 4] : max;
+                            }
+                        }
+                        return max;
+                    },
+                    laplacianFilter: function(data, index, width, sizeOfFilter) {
+                        var range = Math.floor(sizeOfFilter / 2);
+                        var result = 0;
+                        var indexOfFilter = 0;
+                        var filter = [  1, 1, 1,
+                                        1, -8, 1,
+                                        1, 1, 1     ];
+
+                        for( var i = -range; i <= range; i ++ ) {
+                            for( var j = -range; j <= range; j ++ ) {
+                                result = result + data[index + (i * width + j) * 4] * filter[indexOfFilter];
+                                indexOfFilter ++;
+                            }
+                        }
+
+                        result = data[index] - result;
+                        return result;
+                    }
                 };
             },
             showImagePanel: function() {
@@ -66,13 +125,14 @@ jQuery(function(){
 
                 return imageData ;
             },
-            calculateConvolution: function(canvas, filter) {
+            calculateConvolution: function(canvas, filterType, sizeOfFilter) {
                 var width = canvas.width;
                 var height = canvas.height;
                 var ctx = canvas.getContext("2d");
                 var inputData = ctx.getImageData(0, 0, width, height).data;
-                var sizeOfFilter = Math.sqrt(filter.length);
                 var newImageData;
+                var minValue = 999999;
+                var dataArray = [];
 
                 //padding
                 var newWidth = width + 2 * ( sizeOfFilter - 1 );
@@ -82,6 +142,10 @@ jQuery(function(){
                 for( var i = 0; i < sizeOfFilter - 1; i ++ ) {
                     for( var j = 0 ; j < newWidth; j ++ ) {
                         var index = (i * newWidth + j) * 4;
+                        dataArray[index] = 0;
+                        dataArray[index+1] = 0;
+                        dataArray[index+2] = 0;
+                        dataArray[index+3] = 255;
                         newImageData.data[index] = 0;
                         newImageData.data[index+1] = 0;
                         newImageData.data[index+2] = 0;
@@ -91,6 +155,10 @@ jQuery(function(){
                 for( var i = sizeOfFilter - 1; i < height + sizeOfFilter - 1; i ++ ) {
                     for( var j = 0; j < sizeOfFilter - 1; j ++ ) {
                         var index = (i * newWidth + j) * 4;
+                        dataArray[index] = 0;
+                        dataArray[index+1] = 0;
+                        dataArray[index+2] = 0;
+                        dataArray[index+3] = 255;
                         newImageData.data[index] = 0;
                         newImageData.data[index+1] = 0;
                         newImageData.data[index+2] = 0;
@@ -105,6 +173,10 @@ jQuery(function(){
                     }
                     for( var j = sizeOfFilter - 1 + width; j < newWidth; j ++ ) {
                         var index = (i * newWidth + j) * 4;
+                        dataArray[index] = 0;
+                        dataArray[index+1] = 0;
+                        dataArray[index+2] = 0;
+                        dataArray[index+3] = 255;
                         newImageData.data[index] = 0;
                         newImageData.data[index+1] = 0;
                         newImageData.data[index+2] = 0;
@@ -114,6 +186,10 @@ jQuery(function(){
                 for( var i = height + sizeOfFilter - 1; i < height + 2 * (sizeOfFilter - 1); i ++ ) {
                     for( var j = 0 ; j < newWidth; j ++ ) {
                         var index = (i * newWidth + j) * 4;
+                        dataArray[index] = 0;
+                        dataArray[index+1] = 0;
+                        dataArray[index+2] = 0;
+                        dataArray[index+3] = 255;
                         newImageData.data[index] = 0;
                         newImageData.data[index+1] = 0;
                         newImageData.data[index+2] = 0;
@@ -125,34 +201,50 @@ jQuery(function(){
                 for( var i = sizeOfFilter - 1; i < newHeight - ( sizeOfFilter - 1 ); i ++ ) {
                     for( var j = sizeOfFilter - 1; j < newWidth - ( sizeOfFilter - 1 ); j ++ ) {
                         var index = (i * newWidth + j) * 4;
-                        newImageData.data[index] = newImageData.data[((i-1)*newWidth+j-1)*4] * filter[0] +
-                            newImageData.data[((i-1)*newWidth+j)*4] * filter[1] + newImageData.data[((i-1)*newWidth+j+1)*4] * filter[2] +
-                            newImageData.data[(i*newWidth+j-1)*4] * filter[3] + newImageData.data[(i*newWidth+j)*4] * filter[4] +
-                            newImageData.data[(i*newWidth+j+1)*4] * filter[5] + newImageData.data[((i+1)*newWidth+j-1)*4] * filter[6] +
-                            newImageData.data[((i+1)*newWidth+j)*4] * filter[7] + newImageData.data[((i+1)*newWidth+j+1)*4] * filter[8] ;
-
-                        newImageData.data[index+1] = newImageData.data[((i-1)*newWidth+j-1)*4+1] * filter[0] +
-                            newImageData.data[((i-1)*newWidth+j)*4+1] * filter[1] + newImageData.data[((i-1)*newWidth+j+1)*4+1] * filter[2] +
-                            newImageData.data[(i*newWidth+j-1)*4+1] * filter[3] + newImageData.data[(i*newWidth+j)*4+1] * filter[4] +
-                            newImageData.data[(i*newWidth+j+1)*4+1] * filter[5] + newImageData.data[((i+1)*newWidth+j-1)*4+1] * filter[6] +
-                            newImageData.data[((i+1)*newWidth+j)*4+1] * filter[7] + newImageData.data[((i+1)*newWidth+j+1)*4+1] * filter[8] ;
-
-                        newImageData.data[index+2] = newImageData.data[((i-1)*newWidth+j-1)*4+2] * filter[0] +
-                            newImageData.data[((i-1)*newWidth+j)*4+2] * filter[1] + newImageData.data[((i-1)*newWidth+j+1)*4+2] * filter[2] +
-                            newImageData.data[(i*newWidth+j-1)*4+2] * filter[3] + newImageData.data[(i*newWidth+j)*4+2] * filter[4] +
-                            newImageData.data[(i*newWidth+j+1)*4+2] * filter[5] + newImageData.data[((i+1)*newWidth+j-1)*4+2] * filter[6] +
-                            newImageData.data[((i+1)*newWidth+j)*4+2] * filter[7] + newImageData.data[((i+1)*newWidth+j+1)*4+2] * filter[8] ;
+                        dataArray[index] = this.filtersProcessor[filterType](newImageData.data, index, newWidth, sizeOfFilter);
+                        dataArray[index+1] = this.filtersProcessor[filterType](newImageData.data, index+1, newWidth, sizeOfFilter);
+                        dataArray[index+2] = this.filtersProcessor[filterType](newImageData.data, index+2, newWidth, sizeOfFilter);
+                        minValue = dataArray[index] < minValue ? dataArray[index] : minValue;
+                        minValue = dataArray[index+1] < minValue ? dataArray[index+1] : minValue;
+                        minValue = dataArray[index+2] < minValue ? dataArray[index+2] : minValue;
                     }
                 }
 
-                canvas.width = newWidth;
-                canvas.height = newHeight;
+                this.demarcate(dataArray, minValue);
+
+                for( var i = sizeOfFilter - 1; i < newHeight - ( sizeOfFilter - 1 ); i ++ ) {
+                    for( var j = sizeOfFilter - 1; j < newWidth - ( sizeOfFilter - 1 ); j ++ ) {
+                        var index = (i * newWidth + j) * 4;
+                        newImageData.data[index] = dataArray[index];
+                        newImageData.data[index+1] = dataArray[index+1];
+                        newImageData.data[index+2] = dataArray[index+2];
+                    }
+                }
+
                 return newImageData;
             },
-            showImage: function(imageData) {
-                var ctx = canvasCTR.el[0].getContext("2d") ;
+            demarcate : function(data, minValue) {
+                var length = data.length;
+                var maxInterval = 0;
 
-                ctx.putImageData(imageData, 0, 0) ;
+                for( var i = 0; i < length; i ++ ) {
+                    if( maxInterval < data[i] - minValue && (i + 1) % 4 !== 0 ) {
+                        maxInterval = data[i] - minValue;
+                    }
+                }
+
+//                for( var i = 0; i < length; i ++ ) {
+//                    if( maxInterval < data[i] - minValue && (i + 1) % 4 !== 0 ) {
+//                        data[i] = parseInt( 255 * ( (data[i] - minValue) / maxInterval ) );
+//                    }
+//                }
+            },
+            showImage: function(imageData, x, y) {
+                x = x || 0;
+                y = y || 0;
+
+                var ctx = canvasCTR.el[0].getContext("2d") ;
+                ctx.putImageData(imageData, x, y) ;
             },
             processImage: function() {
                 var targetGrayLevel = this.elements[0].val();
@@ -166,8 +258,9 @@ jQuery(function(){
                 }
 
                 if( typeOfFilter !== "" ) {
-                    imageData = this.calculateConvolution(canvas, this.filters[typeOfFilter]);
-                    this.showImage(imageData);
+                    var sizeOfFilter = Math.sqrt( 9 );
+                    imageData = this.calculateConvolution(canvas, typeOfFilter, sizeOfFilter);
+                    this.showImage(imageData, -sizeOfFilter+1, -sizeOfFilter+1);
                 }
             }
         }).init();
